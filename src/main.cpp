81a55848b347bcd2aebc09d77a771b473926c1c1
John Purcell <john@caveofprogramming.com>
2022-05-16 17:29:27 +0200

    C02010 A Single Neuron

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+
+void neuronDemo();
 
 int main() {
-  std::cout << "Hello world" << std::endl;
+  neuronDemo();
   return 0;
+}
+
+double singleNeuron(std::vector<double> inputs, std::vector<double> weights,
+                    double bias) {
+  double weightedSum = 0.0;
+
+  for (int i = 0; i < inputs.size(); ++i) {
+    weightedSum += inputs[i] * weights[i];
+  }
+
+  weightedSum += bias;
+
+  return weightedSum;
+}
+
+void neuronDemo() {
+
+  std::vector<double> inputs{0.2, -0.1, 0.4};
+  std::vector<double> weights{0.3, 0.2, -0.3};
+  double bias = 0;
+
+  std::cout << singleNeuron(inputs, weights, bias) << std::endl;
 }
