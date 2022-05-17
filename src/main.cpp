3918ba9f75aa4e76402182f03ecc0905fb0813c9
John Purcell <john@caveofprogramming.com>
2022-05-17 16:17:51 +0200

    C02030 Coding a Simple Function

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 void neuronDemo();
+void gradientDemo();
 
 int main() {
-  neuronDemo();
+  gradientDemo();
   return 0;
+}
+
+double gradient(double &x, std::function<double()> func) {
+  return 0.0;
+}
+
+void gradientDemo() {
+  double x = 2.0;
+
+  auto func = [x]{
+    double y = 0.5 * x;
+    y *= y;
+    y -= 4;
+
+    return y;
+  };
+
+  std::cout << "x: " << x << std::endl;
+  std::cout << "func: " << func() << std::endl;
+  std::cout << "gradient: " << gradient(x, func) << std::endl;
 }
 
 double singleNeuron(std::vector<double> inputs, std::vector<double> weights,
