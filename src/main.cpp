d03d38efff2c782b7ad3d46eb02e20eacbe79280
John Purcell <john@caveofprogramming.com>
2022-05-22 14:19:16 +0200

    C02070 Loss Functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,84 @@
 #include <functional>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 void neuronDemo();
 void gradientDemo();
 
-int main() {
-  neuronDemo();
-  return 0;
+int main()
+{
+    neuronDemo();
+    return 0;
 }
 
-double gradient(double &x, std::function<double()> func) {
-
-  const double inc = 0.0001;
-  const double originalValue = x;
-
-  double y1 = func();
-
-  x += inc;
-
-  double y2 = func();
-
-  x = originalValue;
-
-  double rate = (y2 - y1) / inc;
-
-  return rate;
+double loss(double actual, double expected) 
+{
+    return std::pow(actual - expected, 2);
 }
 
-void gradientDemo() {
-  double x = -6;
+double gradient(double &x, std::function<double()> func)
+{
+    const double inc = 0.0001;
+    const double originalValue = x;
 
-  auto func = [&x] {
-    double y = 3.7 * x + 4;
+    double y1 = func();
 
-    return y;
-  };
+    x += inc;
 
-  std::cout << "x: " << x << std::endl;
-  std::cout << "func: " << func() << std::endl;
-  std::cout << "gradient: " << gradient(x, func) << std::endl;
+    double y2 = func();
+
+    x = originalValue;
+
+    double rate = (y2 - y1) / inc;
+
+    return rate;
+}
+
+void gradientDemo()
+{
+    double x = -6;
+
+    auto func = [&x]
+    {
+        double y = 3.7 * x + 4;
+
+        return y;
+    };
+
+    std::cout << "x: " << x << std::endl;
+    std::cout << "func: " << func() << std::endl;
+    std::cout << "gradient: " << gradient(x, func) << std::endl;
 }
 
 double singleNeuron(std::vector<double> inputs, std::vector<double> weights,
-                    double bias) {
-  double weightedSum = 0.0;
+                    double bias)
+{
+    double weightedSum = 0.0;
 
-  for (int i = 0; i < inputs.size(); ++i) {
-    weightedSum += inputs[i] * weights[i];
-  }
+    for (int i = 0; i < inputs.size(); ++i)
+    {
+        weightedSum += inputs[i] * weights[i];
+    }
 
-  weightedSum += bias;
+    weightedSum += bias;
 
-  return weightedSum;
+    return weightedSum;
 }
 
-void neuronDemo() {
+void neuronDemo()
+{
 
-  std::vector<double> inputs{0.2, -0.1, 0.4};
-  std::vector<double> weights{0.3, 0.2, -0.3};
-  double bias = 0;
+    std::vector<double> inputs{0.2, -0.1, 0.4};
+    std::vector<double> weights{0.3, 0.2, -0.3};
+    double bias = 0;
 
-  std::cout << singleNeuron(inputs, weights, bias) << std::endl;
+    std::cout << singleNeuron(inputs, weights, bias) << std::endl;
 
-  for (int i = 0; i < inputs.size(); ++i) {
+    for (int i = 0; i < inputs.size(); ++i)
+    {
 
-    // clang-format off
+        // clang-format off
     std::cout << "rate wrt input " << i << ": " << 
       gradient(inputs[i], [&] 
       {
