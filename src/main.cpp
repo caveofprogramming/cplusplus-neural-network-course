837859017ebb6028af8189f654990e8f3d3b3d93
John Purcell <john@caveofprogramming.com>
2022-05-31 23:15:29 +0200

    C05130 How Calculating Errors of Weights Works

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main()
 {
     const int inputSize = 4;
     const int outputSize = 3;
-    const int numberItems = 1;
+    const int numberItems = 2;
 
     auto testData = cave::MatrixFunctions::generateTestData(numberItems, inputSize, outputSize);
 
@@ -21,21 +21,22 @@ int main()
     cave::Matrix &input = testData.input;
     cave::Matrix &expected = testData.output;
 
+    cave::Matrix weight(outputSize, inputSize, [](int i)
+                        { return 0.01 * i * i + i; });
 
-    cave::Matrix weight(outputSize, inputSize, [](int i){ return 0.01 * i * i + i; });
-
-    auto network = [&]{
+    auto network = [&]
+    {
         cave::Matrix result = weight * input;
 
         return cave::MatrixFunctions::meanSquareLoss(result, expected);
     };
 
-    cave::Matrix grad = cave::MatrixFunctions::gradient(weight, network, 0);
+    cave::Matrix grad1 = cave::MatrixFunctions::gradient(weight, network, 0);
+    cave::Matrix grad2 = cave::MatrixFunctions::gradient(weight, network, 1);
 
     std::cout << weight << std::endl;
-    std::cout << grad << std::endl;
-    std::cout << (2.0/outputSize * (weight * input - expected)) * input.transpose() << std::endl;
-    
+    std::cout << grad1 + grad2 << std::endl;
+    std::cout << (2.0 / outputSize * (weight * input - expected)) * input.transpose() << std::endl;
+
     return 0;
 }
-
